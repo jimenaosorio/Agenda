5c38f630322d34485a6eacab38a2838f14e3132c
jimenaosorio <jimena.b.osorio@gmail.com>
2022-11-10 06:06:04 -0300

    Agenda con Firebase, versión 2022

---
+++ app/src/main/java/cl/ejemplos/agenda/modelo/Persona.java
@@ -0,0 +1,57 @@
+package cl.ejemplos.agenda.modelo;
+
+public class Persona {
+    private String uid;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String password;
+
+    public Persona() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
